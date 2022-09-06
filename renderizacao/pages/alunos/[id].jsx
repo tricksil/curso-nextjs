3eb9345ec4917b66025e9b5e9b0ed7cb263dbf03
Patrick Silva <patrickeeep12@gmail.com>
2022-09-06 08:39:04 -0300

    feat: add async paths on getStaticPaths

--- renderizacao/pages/alunos/[id].jsx
+++ renderizacao/pages/alunos/[id].jsx
@@ -1,24 +1,40 @@
-export function getStaticPaths() {
+export async function getStaticPaths() {
+  const resp = await fetch(`http://localhost:3000/api/alunos/tutores`)
+  const ids = await resp.json()
+  const paths = ids.map(id => ({
+    params: {
+      id: `${id}`,
+    },
+  }))
+
   return {
     fallback: false,
-    paths: [
-      { params: { id: '107' } },
-      { params: { id: '203' } },
-      { params: { id: '1345' } },
-    ],
+    paths,
   }
 }
 
-export function getStaticProps() {
+export async function getStaticProps(context) {
+  const resp = await fetch(
+    `http://localhost:3000/api/alunos/${context.params.id}`
+  )
+  const aluno = await resp.json()
+
   return {
-    props: {},
+    props: { aluno },
   }
 }
 
-export default function AlunoPorId() {
+export default function AlunoPorId(props) {
+  const { aluno } = props
+
   return (
     <div>
       <h1>Detalhes do Aluno</h1>
+      <ul>
+        <li>{aluno.id}</li>
+        <li>{aluno.nome}</li>
+        <li>{aluno.email}</li>
+      </ul>
     </div>
   )
 }
