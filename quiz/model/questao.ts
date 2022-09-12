b46ef84b2cc0ea94bbdea4383c3bc8ef50fb6c1e
Patrick Silva <patrickeeep12@gmail.com>
2022-09-12 16:41:00 -0300

    feat: convert to object

--- quiz/model/questao.ts
+++ quiz/model/questao.ts
@@ -41,4 +41,13 @@ export default class QuestaoModel {
     }
     return false
   }
+
+  paraObjeto() {
+    return {
+      id: this.#id,
+      enunciado: this.#enunciado,
+      respostas: this.#respostas.map(resp => resp.paraObjeto()),
+      acertou: this.#acertou,
+    }
+  }
 }
