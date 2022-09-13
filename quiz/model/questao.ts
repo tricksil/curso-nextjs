11a7af87f84845e71dc02202931c79a1a98f2d9c
Patrick Silva <patrickeeep12@gmail.com>
2022-09-13 08:54:31 -0300

    feat: answer the question

--- quiz/model/questao.ts
+++ quiz/model/questao.ts
@@ -43,9 +43,16 @@ export default class QuestaoModel {
     return false
   }
 
-  // responderCom(indice: number): QuestaoModel {
+  responderCom(indice: number): QuestaoModel {
+    const acertou = this.#respostas[indice]?.certa
+    const respostas = this.#respostas.map((resposta, i) => {
+      const respostaSelecionada = indice === i
+      const deveRevelar = respostaSelecionada || resposta.certa
+      return deveRevelar ? resposta.revelar() : resposta
+    })
 
-  // }
+    return new QuestaoModel(this.id, this.enunciado, respostas, acertou)
+  }
 
   embaralharRespostas() {
     let respostasEmbaralhadas = embaralhar(this.#respostas)
@@ -62,6 +69,7 @@ export default class QuestaoModel {
       id: this.#id,
       enunciado: this.#enunciado,
       respostas: this.#respostas.map(resp => resp.paraObjeto()),
+      respondida: this.respondida,
       acertou: this.#acertou,
     }
   }
