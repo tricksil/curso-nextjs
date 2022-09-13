6a55db0fc1d5795120d26e0a02c0dce24ac0f0b2
Patrick Silva <patrickeeep12@gmail.com>
2022-09-13 10:42:28 -0300

    feat: create letter logic

--- quiz/components/Questao.tsx
+++ quiz/components/Questao.tsx
@@ -3,6 +3,13 @@ import QuestaoModel from '../model/questao'
 import Enunciado from './Enunciado'
 import Resposta from './Resposta'
 
+const letras = [
+  { valor: 'A', cor: '#F2C866' },
+  { valor: 'B', cor: '#F266BA' },
+  { valor: 'C', cor: '#85D4F2' },
+  { valor: 'D', cor: '#BCE596' },
+]
+
 interface QuestaoProps {
   valor: QuestaoModel
 }
@@ -17,8 +24,8 @@ export default function Questao(props: QuestaoProps) {
           key={i}
           valor={resposta}
           indice={i}
-          letra='A'
-          corFundoLetra='#F2C866'
+          letra={letras[i].valor}
+          corFundoLetra={letras[i].cor}
         />
       )
     })
