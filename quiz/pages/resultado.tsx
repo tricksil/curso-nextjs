f0430d97eb36bfbd3019907b7bf1f49d7a0a6a91
Patrick Silva <patrickeeep12@gmail.com>
2022-09-22 08:56:08 -0300

    chore: create styles to result

--- quiz/pages/resultado.tsx
+++ quiz/pages/resultado.tsx
@@ -1,7 +1,19 @@
+import styles from '../styles/Resultado.module.css'
+import { useRouter } from 'next/router'
+
 export default function Resultado() {
+  const router = useRouter()
+
+  const total = +router.query.total
+  const certas = +router.query.certas
+  const percentual = Math.round((certas / total) * 100)
+
   return (
-    <div>
+    <div className={styles.resultado}>
       <h1>Resultado</h1>
+      <div>{total}</div>
+      <div>{certas}</div>
+      <div>{`${percentual}%`}</div>
     </div>
   )
 }
