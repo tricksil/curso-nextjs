6f31fa093eb786b4beb2b7886ea3e6722d7511a6
Patrick Silva <patrickeeep12@gmail.com>
2022-11-06 14:22:21 -0300

    feat statistics components

--- quiz/pages/resultado.tsx
+++ quiz/pages/resultado.tsx
@@ -1,5 +1,7 @@
 import styles from '../styles/Resultado.module.css'
 import { useRouter } from 'next/router'
+import Estatistica from '../components/Estatistica'
+import Botao from '../components/Botao'
 
 export default function Resultado() {
   const router = useRouter()
@@ -10,10 +12,17 @@ export default function Resultado() {
 
   return (
     <div className={styles.resultado}>
-      <h1>Resultado</h1>
-      <div>{total}</div>
-      <div>{certas}</div>
-      <div>{`${percentual}%`}</div>
+      <h1>Resultado Final</h1>
+      <div style={{ display: 'flex' }}>
+        <Estatistica texto='Perguntas' valor={total} />
+        <Estatistica texto='Certas' valor={certas} corFundo='#9CD2A4' />
+        <Estatistica
+          texto='Percentual'
+          valor={`${percentual}%`}
+          corFundo='#DE6A33'
+        />
+      </div>
+      <Botao href='/' texto='Tentar Novamente' />
     </div>
   )
 }
