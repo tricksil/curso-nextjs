5c09aa038a7d6c65ad0f189bca4cb075e82def36
Patrick Silva <patrickeeep12@gmail.com>
2022-11-06 15:07:46 -0300

    fix: add env variable

--- quiz/pages/index.tsx
+++ quiz/pages/index.tsx
@@ -3,7 +3,7 @@ import Questionario from '../components/Questionario'
 import QuestaoModel from '../model/questao'
 import { useRouter } from 'next/router'
 
-const BASE_URL = process.env.NEXT_PUBLIC_API
+const BASE_URL = 'curso-nextjs-kaxq.vercel.app/api'
 
 export default function Home() {
   const router = useRouter()
