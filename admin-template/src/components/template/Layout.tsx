43532f43691dd639fb6ff81194c443c5eb3b8999
Patrick Silva <patrickeeep12@gmail.com>
2022-11-17 09:26:30 -0300

    feat: create MenuItem component

--- admin-template/src/components/template/Layout.tsx
+++ admin-template/src/components/template/Layout.tsx
@@ -15,7 +15,7 @@ export default function Layout(props: LayoutProps) {
       <div
         className={`
           flex flex-col w-full p-7 
-          bg-gray-300
+          bg-gray-300 dark:bg-gray-800
         `}
       >
         <Cabecalho titulo={props.titulo} subtitulo={props.subtitulo} />
