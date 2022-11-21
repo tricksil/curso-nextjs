527d6fcb165cff1fa9802ea990a9774dd9b616d7
Patrick Silva <patrickeeep12@gmail.com>
2022-11-21 10:36:42 -0300

    feat: create Formulario component

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Botao from '../components/Botao'
+import Formulario from '../components/Formulario'
 import Layout from '../components/Layout'
 import Tabela from '../components/Tabela'
 import Cliente from '../core/Cliente'
@@ -38,6 +39,7 @@ export default function Home() {
           clienteSelecionado={clienteSelecionado}
           clienteExcluido={clienteExcluido}
         ></Tabela>
+        <Formulario cliente={clientes[0]} />
       </Layout>
     </div>
   )
